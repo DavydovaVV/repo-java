69b50b88a1e28b68f9f2ad54de4bc260bd2a3e3d
DavydovaVV <victoria_davydova@epam.com>
2021-03-25 22:30:59 +0400

    I am deleting excessive empty lines and adding spaces in the comments

--- src/main/java/Davydova/files/StringDemo.java
+++ src/main/java/Davydova/files/StringDemo.java
@@ -9,38 +9,38 @@ import java.util.Arrays;
 
 public class StringDemo {
 
-	/** 
+	/**
 	* 1 Получить длину строки
-	*@param string is the string to get the length of
+	* @param string is the string to get the length of
 	*/
-        public void getLength (String string) {
-        	System.out.println("1 Получить длину строки \n" + "Длина строки = " + string.length());
+	public void getLength (String string) {
+		System.out.println("1 Получить длину строки \n" + "Длина строки = " + string.length());
 	}
 	
 	/** 
 	* 2 Сравнить 2 строки без учета регистра
-	*@param string1 is the string to be compared
-	*@param string2 is the string to be compared
+	* @param string1 is the string to be compared
+	* @param string2 is the string to be compared
 	*/
-        public void compareStrings (String string1, String string2) {
-        	System.out.println("2 Сравнить 2 строки без учета регистра\n" + string1.toLowerCase().equals(string2.toLowerCase()));
+	public void compareStrings (String string1, String string2) {
+		System.out.println("2 Сравнить 2 строки без учета регистра\n" + string1.toLowerCase().equals(string2.toLowerCase()));
 	}
 	
 	/** 
-	*3 Создать новую строку с помощью конструктора и занести ее в пул литералов
-	*@param string is the string to be added to String Pool
+	* 3 Создать новую строку с помощью конструктора и занести ее в пул литералов
+	* @param string is the string to be added to String Pool
 	*/
-        public void addToStringPool (String string) {
-        	System.out.println("3 Создать новую строку с помощью конструктора и занести ее в пул литералов");
+	public void addToStringPool (String string) {
+		System.out.println("3 Создать новую строку с помощью конструктора и занести ее в пул литералов");
 		string.intern();
-        	System.out.println("Строка:\n " + string + "\n внесена в пул строк");
+		System.out.println("Строка:\n " + string + "\n внесена в пул строк");
 	}
-	
+
 	/** 
-	*4 Получить из строки массив символов
-	*@param string is the string to get array of chars of
+	* 4 Получить из строки массив символов
+	* @param string is the string to get array of chars of
 	*/
-        public void getArrayOfChars (String string) {
+	public void getArrayOfChars (String string) {
 		char[] charArray = new char[string.length()];
 		System.out.println("4 Получить из строки массив символов, строка: " + string);
 		for (int i = 0; i < string.length(); i++) {
@@ -53,8 +53,8 @@ public class StringDemo {
 	}
 	
 	/** 
-	*5 Получить из строки массив байтов
-	*@param string is the string to get array of bytes of
+	* 5 Получить из строки массив байтов
+	* @param string is the string to get array of bytes of
 	*/
 	public void getArrayOfBytes (String string) {
 		byte[] byteArray = string.getBytes();
@@ -63,19 +63,19 @@ public class StringDemo {
 	}
 	
 	/** 
-	*6 Привести строку к верхнему регистру
-	*@param string is the string to be brought to upper case
+	* 6 Привести строку к верхнему регистру
+	* @param string is the string to be brought to upper case
 	*/
 	public void getToUpperCase (String string) {
-        	System.out.println ("6 Привести строку к верхнему регистру\n" + string.toUpperCase());
+		System.out.println ("6 Привести строку к верхнему регистру\n" + string.toUpperCase());
 	}
 	
 	/** 
-	*7 Найти первую позицию символа "a" в строке
-	*@param string is the string to be tested
-	*@param a is the char to be found its frist occurance in the string
+	* 7 Найти первую позицию символа "a" в строке
+	* @param string is the string to be tested
+	* @param a is the char to be found its frist occurance in the string
 	*/
-        public void findFirstPosition (String string, char a) {
+	public void findFirstPosition (String string, char a) {
 		System.out.println ("7 Найти первую позицию символа \"a\" в строке\n" + string);
 		int indexFirst = string.indexOf("а");
 		if (indexFirst == -1) System.out.println("Символ \"a\" не найден");
@@ -83,11 +83,11 @@ public class StringDemo {
 	}
 	
 	/** 
-	*8 Найти последнюю позицию символа "а" в строке
-	*@param string is the string to be tested
-	*@param a is the char to be found its last occurance in the string
+	* 8 Найти последнюю позицию символа "а" в строке
+	* @param string is the string to be tested
+	* @param a is the char to be found its last occurance in the string
 	*/
-        public void findLastPostion (String string, char a) {
+	public void findLastPostion (String string, char a) {
 		System.out.println ("8 Найти последнюю позицию символа " + a + " в строке\n" + string);
 		int indexLast = string.lastIndexOf("а");
 		if (indexLast == -1) System.out.println ("Символ " + a + " не найден");
@@ -95,15 +95,14 @@ public class StringDemo {
         }
 	
 	/** 
-	*9 Проверить содержит ли строка слово "Sun"
-	*@param string is the string to be tested
-	*@param subString is the string to be found in the string
+	* 9 Проверить содержит ли строка слово "Sun"
+	* @param string is the string to be tested
+	* @param subString is the string to be found in the string
 	*/
-        public void isContained (String string, String subString) {
+	public void isContained (String string, String subString) {
 		boolean foundIt = false;
 		int max = string.length() - subString.length();
 
-		
 		for (int i = 0; i < max; i++) {
 		    int n = subString.length();
 		    int j = 0;
@@ -123,53 +122,53 @@ public class StringDemo {
 	}
 	
 	/** 
-	*10 Проверить оканчивается ли строка на "Oracle"
-	*@param string is the string to be tested
-	*@param subString is the string to be verified if it ends the string
+	* 10 Проверить оканчивается ли строка на "Oracle"
+	* @param string is the string to be tested
+	* @param subString is the string to be verified if it ends the string
 	*/
-        public void isLastSubString (String string, String subString) {
+	public void isLastSubString (String string, String subString) {
 
 		System.out.println ("10 Проверить оканчивается ли строка на " + subString);
 		System.out.println (string.endsWith(subString));
 	}
 	
 	/** 
-	*11 Проверить начинается ли строка на "Java"
-	*@param string is the string to be tested
-	*@param subString is the string to be verified if it starts the string
+	* 11 Проверить начинается ли строка на "Java"
+	* @param string is the string to be tested
+	* @param subString is the string to be verified if it starts the string
 	*/
-        public void isFirstSubString (String string, String subString){
+	public void isFirstSubString (String string, String subString){
 
 		System.out.println ("11 Проверить начинается ли строка на " + subString);
 		System.out.println (string.startsWith(subString));
 	}
 	
 	/** 
-	*12 Заменить все символы "а" в строке на символ "о"
-	*@param string is the string to be modified
-	*@param target is the char to be found and replaced in the string
-	*@param replacement is the char to be inserted instead of target in the string
+	* 12 Заменить все символы "а" в строке на символ "о"
+	* @param string is the string to be modified
+	* @param target is the char to be found and replaced in the string
+	* @param replacement is the char to be inserted instead of target in the string
 	*/
-        public void changeChar (String string, char target, char replacement) {
+	public void changeChar (String string, char target, char replacement) {
 		System.out.println ("12 Заменить все символы " + target + " в строке на символ " + replacement);
 		System.out.println (string.replace(target, replacement));
 	}
 	
 	/** 
-	*13 Получить подстроку с 44 символа по 90 символ
-	*@param string is the string to be tested with method substring() 
+	* 13 Получить подстроку с 44 символа по 90 символ
+	* @param string is the string to be tested with method substring()
 	*/
-        public void getSubString (String string) {
+	public void getSubString (String string) {
 		System.out.println ("13 Получить подстроку с 44 символа по 90 символ");
 		if (string.length() < 90) System.out.println("Длина строки меньше 90 символов");
 		else System.out.println (string.substring(44,90));
 	}
 	
 	/** 
-	*14 Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива)
-	*@param string is the string to get an array by splitting with " "
+	* 14 Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива)
+	* @param string is the string to get an array by splitting with " "
 	*/
-        public void splitString (String string) {
+	public void splitString (String string) {
 		String[] colors = string.split(" ");
 
 		System.out.println ("14 Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива)");
@@ -179,10 +178,10 @@ public class StringDemo {
 	}
 	
 	/** 
-	*15 Поменять последовательность символов в строке на обратную
-	*@param string is the string to be reversed
+	* 15 Поменять последовательность символов в строке на обратную
+	* @param string is the string to be reversed
 	*/
-        public void changeOrder (String string) {
+	public void changeOrder (String string) {
 		char[] reverse = new char[string.length()];
 		int j = 0;
 		for (int i = string.length()-1; i >= 0; i--) {
