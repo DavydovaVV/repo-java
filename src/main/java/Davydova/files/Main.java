4cbe24c3141e8ee7090b7e516a753e2e17eaa83b
DavydovaVV <81247195+DavydovaVV@users.noreply.github.com>
2021-04-02 16:16:32 +0400

    Revert mistaken commit

--- src/main/java/Davydova/files/Main.java
+++ src/main/java/Davydova/files/Main.java
@@ -1,15 +1,6 @@
 public class Main {
 	
 	public static void main (String[] args) {
-		Cache<Integer> cache = new Cache<>(3);
-		cache.add(6,3);
-		cache.add(5,2);
-		cache.add(7,6);
-		cache.isPresentElement(6);
-		cache.delete(6);
-		cache.isPresentIndex(3);
-		cache.isPresentIndex(2);
-		cache.get(7);
-		cache.clear();
+		
 	}
 }
