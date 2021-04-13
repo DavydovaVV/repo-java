7ae1d1195d4e260d0e8d540c57bd22140a75f80d
DavydovaVV <victoria_davydova@epam.com>
2021-04-13 15:25:26 +0400

    Added CacheTest and StorageTest.

--- src/test/java/davydova/files/StorageTest.java
+++ src/test/java/davydova/files/StorageTest.java
@@ -1,7 +1,118 @@
 package davydova.files;
 
+import Davydova.files.Cache;
+import Davydova.files.Storage;
+import org.junit.Test;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class StorageTest {
 
+public class StorageTest {
+
+    private String[] smallArray = {"1", "2", "3"};
+    private String[] defaultSizeArray = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    private Storage<String> smallStorage = new Storage<>(smallArray);
+    private Storage<String> defaultSizeStorage = new Storage<>(defaultSizeArray);
+
+    @Test
+    public void testGetStorageArray() {
+        smallStorage.getStorage();
+
+        String expected = Arrays.toString(smallArray);
+        String actual = Arrays.toString((Arrays.copyOfRange(smallStorage.getStorage(), 0, 3)));
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void addElementToNotFullStorageArray() {
+        smallStorage.add("4");
+
+        String expected = "4";
+        String actual = smallStorage.getStorage()[3].toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addElementToFullStorageArray() {
+        defaultSizeStorage.add("10");
+
+        System.out.println(Arrays.toString(defaultSizeStorage.getStorage()));
+        String expected = "10";
+        String actual = defaultSizeStorage.getStorage()[10].toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncreaseStorageArray() {
+        Object[] increasedArray = defaultSizeStorage.increaseArray(defaultSizeStorage.getStorage());
+
+        int expected = 15;
+        int actual = increasedArray.length;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeleteLastElementFromStorageArray() {
+        smallStorage.getCache().add("3", 11);
+        smallStorage.delete();
+
+        assertTrue(smallStorage.getStorage()[2] == null);
+    }
+
+    @Test
+    public void testClear() {
+        Storage<String> checkStorage = new Storage<>();
+        Cache<String> checkCache = new Cache<>(10);
+
+        defaultSizeStorage.clear();
+
+        assertEquals(Arrays.toString(checkStorage.getStorage()),
+                Arrays.toString(defaultSizeStorage.getStorage()));
+
+        assertEquals(Arrays.toString(checkCache.getCache()),
+                Arrays.toString(defaultSizeStorage.getCache().getCache()));
+    }
+
+    @Test
+    public void getLastNullElementFromEmptyStorageArray() {
+        Storage<String> emptyStorage = new Storage<>();
+        System.out.println(emptyStorage.getLast());
+
+        assertNull(emptyStorage.getLast());
+    }
+
+    @Test
+    public void getLastNotNullElementFromStorageArray() {
+        String expected = "9";
+        String actual = defaultSizeStorage.getLast();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPresentElementFromStorageArray() {
+        smallStorage.getCache().add("3", 11);
+
+        String expected = "3";
+        String actual = smallStorage.get(2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNotPresentElementInCacheFromStorageArray() {
+        String expected = "2";
+        String actualElementInStorage = smallStorage.get(1);
+        String actualElementAddedToCache = smallStorage.getCache().getCache()[0].getElement();
+
+        assertEquals(expected, actualElementInStorage);
+        assertEquals(expected, actualElementAddedToCache);
+    }
 }
