318ce1cb834f07940d0518f59a2ccba5d535f743
DavydovaVV <victoria_davydova@epam.com>
2021-04-13 16:42:42 +0400

    Added Test classes for Cache and Storage

---
+++ src/test/java/davydova/files/CacheTest.java
@@ -0,0 +1,146 @@
+package davydova.files;
+
+import Davydova.files.Cache;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import java.util.Arrays;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@Slf4j
+public class CacheTest<T> {
+
+    private Cache<String> cache = new Cache<>(3);
+
+    @Test
+    public void testGetCache() {
+        String[] checkArray = new String[3];
+
+        String expected = Arrays.toString(checkArray);
+        String actual = Arrays.toString(cache.getCache());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addNewElementToFullCacheElementArray() {
+        cache.add("один", 0);
+        cache.add("два", 1);
+        cache.add("три", 2);
+        cache.add("newElement", 3);
+
+        assertEquals("newElement", cache.getCache()[2].getElement());
+    }
+
+    @Test
+    public void addNewElementToEmptyCacheElementArray() {
+        cache.add("newElement", 0);
+
+        assertEquals("newElement", cache.getCache()[0].getElement());
+    }
+
+    @Test
+    public void addNewElementToNotEmptyCacheElementArray() {
+        cache.add("один", 0);
+        cache.add("newElement", 1);
+
+        assertEquals("newElement", cache.getCache()[1].getElement());
+    }
+
+    @Test
+    public void deleteElementFromNotEmptyCacheElementArray() {
+        cache.add("один", 0);
+        cache.add("два", 1);
+        cache.add("три", 2);
+        cache.delete("два");
+
+        String expected = "два";
+        String actual = cache.getCache()[1].getElement();
+
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void isPresentElementInNotEmptyCacheElementArray() {
+        cache.add("presentElement", 5);
+        cache.add("otherElement", 1);
+
+        boolean expected = true;
+        boolean actual = cache.isPresent("presentElement");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void isPresentElementInNullCacheElementArray() {
+
+        assertTrue(null == cache.getCache()[0].getElement());
+    }
+
+    @Test
+    public void isPresentIndexInNotEmptyCacheElementArray() {
+        cache.add("presentElement", 5);
+        cache.add("otherElement", 1);
+        cache.isPresent(5);
+
+        boolean expected = true;
+        boolean actual = cache.isPresent(5);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getElementAndPlaceAtTheEndOfNotFullCacheElementArray() {
+        cache.add("presentElement", 5);
+        cache.add("otherElement", 1);
+
+        String expectedElement = "presentElement";
+        String actualResultElement = cache.get(5).getElement();
+
+        int expectedIndex = 5;
+        int actualIndex = cache.getCache()[1].getIndex();
+
+        assertEquals(expectedElement, actualResultElement);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void getElementAndPlaceAtTheEndOfFullCacheElementArray() {
+        cache.add("oneElement", 5);
+        cache.add("otherElement", 1);
+        cache.add("presentElement", 10);
+
+        String expectedElement = "presentElement";
+        String actualResultElement = cache.get(10).getElement();
+
+        int expectedIndex = 10;
+        int actualIndex = cache.getCache()[2].getIndex();
+
+        assertEquals(expectedElement, actualResultElement);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getNotPresentElementAndPlaceAtTheEndOfCacheElementArray() {
+        cache.add("oneElement", 5);
+        cache.add("otherElement", 1);
+
+        cache.get(6).getElement();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getNotPresentElementAndPlaceAtTheEndOfEmptyCacheElementArray() {
+        cache.get(0).getElement();
+    }
+
+    @Test
+    public void testClear() {
+        Cache<String> checkCache = new Cache<>(3);
+        cache.clear();
+
+        assertEquals(Arrays.toString(checkCache.getCache()), Arrays.toString(cache.getCache()));
+    }
+}
