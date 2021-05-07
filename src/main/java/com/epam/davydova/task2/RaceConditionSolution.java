68a47a3cf80865d99e640ff3e446670f28fbfc0f
DavydovaVV <81247195+DavydovaVV@users.noreply.github.com>
2021-05-07 16:25:04 +0400

    Task1 - Task3 Concurrency (#14)
    * Task1 - Task3 Concurrency
    
    * Task3 fixed
    
    * Fixed as per comments

---
+++ src/main/java/com/epam/davydova/task2/RaceConditionSolution.java
@@ -0,0 +1,46 @@
+package com.epam.davydova.task2;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * This is a class of solution for Race condition from task1
+ */
+@Slf4j
+public class RaceConditionSolution {
+    private final ArrayList<Integer> arrayList = new ArrayList<>();
+
+    /**
+     * Calculate mismatch between actual and expected ArrayList size
+     */
+    public void calculateMismatch() {
+        Thread thread1 = new Thread(this::fillArrayList);
+        Thread thread2 = new Thread(this::fillArrayList);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            log.error("Exception is: ", e);
+        }
+
+        log.info("ArrayList size mismatch after filling up with 2000 elements " +
+                "using 2 synchronous threads = {}", 2000 - arrayList.size());
+    }
+
+    /**
+     * Fill an ArrayList with 1000 random elements
+     */
+    private synchronized void fillArrayList() {
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(arrayList.size(), new Random().nextInt(10));
+
+            log.debug("Index [{}] of ArrayList was filled with an element by current thread", arrayList.size() - 1);
+        }
+    }
+}
---
+++ src/main/java/com/epam/davydova/task3/Chat.java
@@ -0,0 +1,99 @@
+package com.epam.davydova.task3;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * This is a class that represents work of chat
+ */
+@Slf4j
+public class Chat {
+    private BlockingQueue<String> smsQueue = new ArrayBlockingQueue<>(25);
+
+    /**
+     * Write a message to chat
+     *
+     * @param numberOfThreads is a number of threads that write messages
+     */
+    public void writeMessage(int numberOfThreads) {
+        var service = Executors.newScheduledThreadPool(numberOfThreads);
+
+        for (var i = 0; i < numberOfThreads; i++) {
+            service.scheduleAtFixedRate(() -> {
+                var message = "id:" + new Random().nextInt(100);
+
+                try {
+                    smsQueue.put(message);
+                } catch (InterruptedException e) {
+                    log.error("Exception is: ", e);
+                }
+
+                if (smsQueue.size() == 25) {
+                    log.debug("Chat capacity is loaded. Message will not be written until read");
+                }
+
+                log.info("Thread, has written the message \"[{}]\" to the Chat. " +
+                                "Current capacity of the Chat is [{}]", message, smsQueue.size());
+
+            }, 0, 20 + new Random().nextInt(39), TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Read a message from chat
+     *
+     * @param numberOfThreads is a number of threads that read messages from chat
+     */
+    public void readMessage(int numberOfThreads) {
+        var service = Executors.newScheduledThreadPool(numberOfThreads);
+
+        for (var i = 0; i < numberOfThreads; i++) {
+            service.scheduleAtFixedRate(() -> {
+                String message = "";
+
+                try {
+                    message = smsQueue.take();
+                } catch (InterruptedException e) {
+                    log.error("Exception is: ", e);
+                }
+
+                log.info("Thread, has read the message \"[{}]\" from the Chat. " +
+                                "Current capacity of the Chat is [{}]", message, smsQueue.size());
+            }, 0, 30 + new Random().nextInt(19), TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Modify and return message to chat
+     *
+     * @param numberOfThreads is a number of threads that modify messages
+     */
+    public void modifyMessage(int numberOfThreads) {
+        var service = Executors.newScheduledThreadPool(numberOfThreads);
+
+        for (var i = 0; i < numberOfThreads; i++) {
+            service.scheduleAtFixedRate(() -> {
+                var message = "";
+                var modifiedMessage = "";
+
+                try {
+                    message = smsQueue.take();
+                    modifiedMessage = message + new Random().nextInt(10);
+                    smsQueue.put(modifiedMessage);
+                } catch (InterruptedException e) {
+                    log.error("Exception is: ", e);
+                }
+
+                if (smsQueue.size() == 25) {
+                    log.debug("Chat capacity is loaded. Message will not be written until read");
+                }
+
+                log.info("Thread, has modified the message \"[{}]\" from Chat. " +
+                                "Modified message is \"[{}]\". Current capacity of the Chat is [{}]",
+                        message, modifiedMessage, smsQueue.size());
+            }, 0, 65, TimeUnit.SECONDS);
+        }
+    }
+}
