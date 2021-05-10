ad8e993b9d267e39c5f73b1b6dc354cfea1e637f
DavydovaVV <81247195+DavydovaVV@users.noreply.github.com>
2021-05-10 23:13:55 +0400

    Added modules: api, db, impl (#15)
    * Added modules: api, db, impl
    
    * fixed as per comments
    
    * fixed as per comments modules' pom.xml
    
    * correct groupId
    
    * groupId + artefactId
    
    * groupId + artefactId (final)
    
    * groupId + artefactId (final)
    
    * added another package
    
    * added missing files

--- assignment-api/src/main/java/com/epam/rd/davydova/assignment/Main.java
+++ assignment-api/src/main/java/com/epam/rd/davydova/assignment/Main.java
@@ -1,4 +1,4 @@
-package com.epam.davydova;
+package com.epam.rd.davydova.assignment;
 
 import lombok.extern.slf4j.Slf4j;
 
---
+++ assignment-api/src/test/java/com/epam/rd/davydova/assignment/MainTest.java
@@ -0,0 +1,8 @@
+package com.epam.rd.davydova.assignment;
+
+class MainTest {
+    public static void main(String[] args) {
+
+    }
+
+}
