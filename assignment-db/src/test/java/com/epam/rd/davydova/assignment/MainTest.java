fe4a50a5325b5b5e3f46e7487b38954ed75b455b
DavydovaVV <victoria_davydova@epam.com>
2021-05-10 22:41:05 +0400

    added another package

---
+++ assignment-db/src/test/java/com/epam/rd/davydova/assignment/MainTest.java
@@ -0,0 +1,7 @@
+package com.epam.rd.davydova.assignment;
+
+class MainTest {
+    public static void main(String[] args) {
+
+    }
+}
--- davydova-api/src/test/java/com/epam/rd/davydova/MainTest.java
+++
@@ -1,8 +0,0 @@
-package com.epam.rd.davydova;
-
-class MainTest {
-    public static void main(String[] args) {
-
-    }
-
-}
