b5586f7a36d37701e88f12a771f167c18f6b57f4
DavydovaVV <victoria_davydova@epam.com>
2021-05-17 11:20:07 +0400

    changed as per comments

--- assignment-db/src/main/resources/query.sql
+++ assignment-db/src/main/resources/query.sql
@@ -9,7 +9,7 @@ order by category, price;
 
 select * 
 from products
-where category = 8 or category = 15;
+where category in(8, 15);
 
 select * 
 from products
@@ -17,22 +17,25 @@ where price between 10 and 20;
 
 select * 
 from orders
-where orderdate between '2004-01-05' and '2004-02-05';
+where orderdate between to_date('2004-01-05', 'yyyy-mm-dd') and to_date('2004-02-05', 'yyyy-mm-dd');
 
 select customerid, 
-count(*)
+count(*) as "quantity of customers"
 from orders 
 group by customerid
-order by 1, 2;
+order by customerid, 
+count(*);
 
 select orderdate, 
 customerid, 
-sum(totalamount) 
+sum(totalamount) as "total sum" 
 from orders
 group by orderdate, 
 customerid 
 having sum(totalamount) > 100
-order by 1, 2, 3;
+order by orderdate, 
+customerid, 
+sum(totalamount);
 
 select orders.orderdate,
 products.title,
@@ -42,6 +45,9 @@ from  orderlines
 inner join orders on orderlines.orderid = orders.orderid
 inner join customers on orders.customerid = customers.customerid
 inner join products on orderlines.prod_id = products.prod_id
-where orderlines.orderdate < '2004-01-07'
-order by 1, 2, 3, 4;
+where orderlines.orderdate < to_date('2004-01-07', 'yyyy-mm-dd')
+order by orders.orderdate,
+products.title,
+customers.firstname, 
+customers.lastname;
 
