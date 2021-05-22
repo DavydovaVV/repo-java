b941c4e96d7e045a01383c913a091edda65e1f42
DavydovaVV <victoria_davydova@epam.com>
2021-05-22 19:56:37 +0400

    rolled back main

--- assignment-impl/src/main/java/com/epam/rd/davydova/assignment/Main.java
+++ assignment-impl/src/main/java/com/epam/rd/davydova/assignment/Main.java
@@ -1,6 +1,5 @@
 package com.epam.rd.davydova.assignment;
 
-import com.epam.rd.davydova.assignment.domain.service.ExecutionService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -8,15 +7,5 @@ public class Main {
 	
 	public static void main (String[] args) {
 
-		ExecutionService executionService = new ExecutionService();
-
-		//executionService.addCustomer(new Customer("Wynnie Ramsdale"));
-
-		/*var order = new Order("001",
-				new Customer("Doti Fretter"),
-				new Date(), BigDecimal.valueOf(12345));
-
-		executionService.addOrder(order);*/
 	}
-	
 }
